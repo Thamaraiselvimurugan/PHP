d24648027179d07c0807f5e58d44eaf3b63a1c6f
Thamaraiselvimurugan <40590355+Thamaraiselvimurugan@users.noreply.github.com>
2018-07-24 13:14:58 +0530

    Add files via upload

---
+++ exp1.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title> PHP 1 </title>
+    </head>
+    <body>
+        <?php
+        $txt1 = "thamarai!!Welcome To Php";
+$txt2 = "Rajalakshmi Institute of Technology";
+$x = 19;
+$y = 400;
+print "<h2>$txt1</h2>";
+print "Study PHP at $txt2<br>";
+print "Sum of two numbers is : ";
+print $x + $y ;
+        ?>
+    </body>
+</html>
+
+
---
+++ exp2.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+     $txt1 = "Welcome To Php";
+        $x = "Hello !Have a nice day";
+$y = 'Hello world!';
+print "<h2><i>$txt1</i></h2>";
+echo $x;
+echo "<br>"; 
+echo $y;
+echo "<br>"; 
+$x = 9999;
+var_dump($x);
+        ?>
+    </body>
+</html>
+
+
